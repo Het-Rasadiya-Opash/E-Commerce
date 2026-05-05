ba630f171516b9f6e1e6952bd46e00ec920ba836
Het <hra.opash@gmail.com>
2026-05-05 12:24:43 +0530

    Get Current User

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Login from "./pages/Login";
@@ -6,10 +6,33 @@ import Home from "./pages/Home";
 import { Route, Routes, useLocation } from "react-router";
 import Register from "./pages/Register";
 import Navbar from "./components/Navbar";
+import { useDispatch } from "react-redux";
+import apiRequest from "./utils/apiRequest";
+import { setCheckingAuth, setCurrentUser } from "./features/usersSlice";
 
 const App = () => {
   const location = useLocation();
   const hideNavbar = ["/login", "/register"].includes(location.pathname);
+
+  const dispatch = useDispatch();
+
+  const hasCheckedAuth = React.useRef(false);
+
+  useEffect(() => {
+    if (hasCheckedAuth.current) return;
+    hasCheckedAuth.current = true;
+
+    const checkAuth = async () => {
+      try {
+        const response = await apiRequest.get("/users");
+        const user = response.data.data;
+        dispatch(setCurrentUser(user));
+      } catch (err) {
+        dispatch(setCheckingAuth(false));
+      }
+    };
+    checkAuth();
+  }, [dispatch]);
 
   return (
     <div className="min-h-screen">
